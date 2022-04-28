1ea42000fcf1fe16863ba9b6b2b05a75bed22e6a
Brandon <brandon.cebi.pro@gmail.com>
2022-04-28 14:18:41 +0200

    add comments

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -18,73 +18,83 @@ class TestController extends AbstractController
     public function index(OrderRepository $orderRepo, OrderEntryRepository $orderEntryRepo, DepositEntryRepository $depositEntryRepo, StockEntryRepository $stockEntryRepo, EntityManagerInterface $em): Response
     {
 
-        $test = $orderRepo->findBy(['id' => 1]);
-        // SI LE PANIER EST VALIDÉ
-        if($test[0]->isValidated() === true)
+        $validePanier = $orderRepo->findBy(['validated' => 1]);
+        if(!empty($validePanier))
         {
-            // RECUPERER LA VALEUR DE L'ID DU PANIER
-            $id_panier = $test[0]->getId();
-            $orderEntry = $orderEntryRepo->findBy(['order' => $id_panier]);
-            // RECUPERER LA VALEUR DE L'ID DU PRODUIT
-            $product_id = $orderEntry[0]->getOrder();
-            // RECUPERER LA QUANTITÉ RESTANTE DU PRODUIT EN QUESTION
-            $quantity = $orderEntry[0]->getQuantity();
-            $depositEntry = $depositEntryRepo->findBy(['product' => $product_id]);
-            $stockEntry = $stockEntryRepo->findBy(['product' => $product_id]);
-
-            $depositEmpty = true;
-
-            foreach($depositEntry as $clientOrder)
+            // SI LE PANIER EST VALIDÉ
+            if($validePanier[0]->isValidated() === true)
             {
+                // RECUPERER LA VALEUR DE L'ID DU PANIER
+                $id_panier = $validePanier[0]->getId();
+                $orderEntry = $orderEntryRepo->findBy(['order' => $id_panier]);
+                // RECUPERER LA VALEUR DE L'ID DU PRODUIT
+                $product_id = $orderEntry[0]->getOrder();
+                // RECUPERER LA QUANTITÉ RESTANTE DU PRODUIT EN QUESTION
+                $quantity = $orderEntry[0]->getQuantity();
+                $depositEntry = $depositEntryRepo->findBy(['product' => $product_id]);
+                $stockEntry = $stockEntryRepo->findBy(['product' => $product_id]);
 
-                if($clientOrder->getQuantity() >= $quantity)
+                $depositEmpty = true;
+
+                // VERIFIE SI IL Y A LES QUANTITÉS REQUISES DANS LE DEPOT
+                foreach($depositEntry as $clientOrder)
                 {
-                    $depositEmpty = false;
-                    break;
+                    if($clientOrder->getQuantity() >= $quantity)
+                    {
+                        $depositEmpty = false;
+                        break;
+                    }
+                    else
+                    {
+                        $depositEmpty = true;
+                    }
                 }
+
+                // SI LES QUANTITÉS REQUISES SONT DANS LE DÉPOT
+                if($depositEmpty === false)
+                {
+                    for($i = 0; $i < Count($depositEntry); $i++)
+                    {
+                        if($depositEntry[$i]->getQuantity() >= $quantity)
+                        {
+                            // UPDATE LA QUANTITE VENDUE
+                            $depositEntry[$i]->setSoldQuantity($depositEntry[$i]->getSoldQuantity() + $quantity);
+                            // UPDATE LA QUANTITE RESTANTE
+                            $depositEntry[$i]->setQuantity($depositEntry[$i]->getQuantity() - $quantity);
+                            $em->flush();
+                            break;
+                        }
+                    }
+                }
+
                 else
                 {
-                    $depositEmpty = true;
-                }
-            }
-
-            if($depositEmpty === false)
-            {
-                for($i = 0; $i < Count($depositEntry); $i++)
-                {
-                    if($depositEntry[$i]->getQuantity() >= $quantity)
+                    for($i = 0; $i < Count($stockEntry); $i++)
                     {
-                        // UPDATE LA QUANTITE VENDUE
-                        $depositEntry[$i]->setSoldQuantity($depositEntry[$i]->getSoldQuantity() + $quantity);
-                        // UPDATE LA QUANTITE RESTANTE
-                        $depositEntry[$i]->setQuantity($depositEntry[$i]->getQuantity() - $quantity);
-                        $em->flush();
-                        break;
+
+                        if($stockEntry[$i]->getQuantity() >=$quantity)
+                        {
+                            $stockEntry[$i]->setSoldQuantity($stockEntry[$i]->getSoldQuantity() + $quantity);
+                            $stockEntry[$i]->setQuantity($stockEntry[$i]->getQuantity() - $quantity);
+                            $em->flush();
+                            break;
+                        }
                     }
                 }
-            }
 
+
+            }
             else
             {
-                for($i = 0; $i < Count($stockEntry); $i++)
-                {
-                    dump($i);
-                    if($stockEntry[$i]->getQuantity() >=$quantity)
-                    {
-                        $stockEntry[$i]->setSoldQuantity($stockEntry[$i]->getSoldQuantity() + $quantity);
-                        $stockEntry[$i]->setQuantity($stockEntry[$i]->getQuantity() - $quantity);
-                        $em->flush();
-                        break;
-                    }
-                }
+                dd('AUCUN PANIER VALIDÉ');
             }
-
-
         }
+
         else
         {
-            dd('false');
+            dd("Aucun Panier");
         }
+
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
         ]);

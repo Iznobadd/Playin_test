a240e1bfb201376dbc0882c1b99271d121b5cd04
Brandon <brandon.cebi.pro@gmail.com>
2022-04-28 12:13:42 +0200

    initial commit

---
+++ src/Controller/TestController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Order;
+use App\Repository\DepositEntryRepository;
+use App\Repository\OrderEntryRepository;
+use App\Repository\OrderRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    #[Route('/test', name: 'app_test')]
+    public function index(OrderRepository $orderRepo, OrderEntryRepository $orderEntryRepo, DepositEntryRepository $depositEntryRepo, EntityManagerInterface $em): Response
+    {
+
+        $test = $orderRepo->findBy(['id' => 1]);
+        // SI LE PANIER EST VALIDÉ
+        if($test[0]->isValidated() === true)
+        {
+            // RECUPERER LA VALEUR DE L'ID DU PANIER
+            $id_panier = $test[0]->getId();
+            $orderEntry = $orderEntryRepo->findBy(['order' => $id_panier]);
+            // RECUPERER LA VALEUR DE L'ID DU PRODUIT
+            $product_id = $orderEntry[0]->getOrder();
+            // RECUPERER LA QUANTITÉ RESTANTE DU PRODUIT EN QUESTION
+            $quantity = $orderEntry[0]->getQuantity();
+            $depositEntry = $depositEntryRepo->findBy(['product' => $product_id]);
+
+            $depositEmpty = true;
+
+            foreach($depositEntry as $clientOrder)
+            {
+
+                if($clientOrder->getQuantity() >= $quantity)
+                {
+                    $depositEmpty = false;
+                    break;
+                }
+                else
+                {
+                    $depositEmpty = true;
+                }
+            }
+
+            if($depositEmpty === false)
+            {
+                for($i = 0; $i < Count($depositEntry); $i++)
+                {
+                    if($depositEntry[$i]->getQuantity() >= $quantity)
+                    {
+                        // UPDATE LA QUANTITE VENDUE
+                        $depositEntry[$i]->setSoldQuantity($depositEntry[$i]->getSoldQuantity() + $quantity);
+                        // UPDATE LA QUANTITE RESTANTE
+                        $depositEntry[$i]->setQuantity($depositEntry[$i]->getQuantity() - $quantity);
+                        $em->flush();
+                        break;
+                    }
+                }
+            }
+
+            else
+            {
+                dd('test');
+            }
+
+
+        }
+        else
+        {
+            dd('false');
+        }
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'TestController',
+        ]);
+    }
+}
